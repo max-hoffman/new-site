23cd3697b9485fcb09d28b0f497b1ce9ab882286
Max Hoffman <maximilian.wolfgang1@gmail.com>
2018-01-16 14:56:15 -0600

    Adds some styling to post page

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,36 @@
 import Layout from '../components/MyLayout.js';
 import { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
+import moment from 'moment';
 
+const styles = {
+    title: {
+        fontSize: 30,
+        fontWeight: 'normal'
+    },
+    date: {
+        fontSize: 15,
+        opacity: '0.8',
+        fontStyle: 'italic',
+        marginTop: 2,
+        marginBottom: 2
+    },
+    body: {
+        fontSize: 18
+    },
+    hr: {
+        marginLeft: 0,
+        marginRight: 0,
+        marginTop: 15,
+        marginBottom: 15
+    }
+}
 const Post = (props) => (
     <Layout>
-       <h1>{props.title}</h1>
-       <p>{props.createdAt}</p>
-       <p>{props.body}</p>
+       <h1 style={styles.title}>{props.title}</h1>
+       <p style={styles.date}>{props.createdAt}</p>
+       <hr style={styles.hr}/>
+       <p style={styles.body}>{props.body}</p>
     </Layout>
 );
 
@@ -18,7 +42,7 @@ Post.getInitialProps = async function({ query }) {
       title: data.title,
       body: data.body,
       id: data.id,
-      createdAt: data.createdAt,
+      createdAt: moment(data.createdAt).format('MMM DD, YYYY'),
       updatedAt: data.updatedAt
     }
   };
