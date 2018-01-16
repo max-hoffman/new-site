a0dc782151e8fdd8dad04d7aa9e7021e99af4473
Max Hoffman <maximilian.wolfgang1@gmail.com>
2018-01-16 14:14:55 -0600

    Automates page loading

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,26 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout.js';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-export default (props) => (
+const Post = (props) => (
     <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>{props.url.query.preview}</p>
+       <h1>{props.title}</h1>
+       <p>{props.createdAt}</p>
+       <p>{props.body}</p>
     </Layout>
-)
+);
+
+Post.getInitialProps = async function({ query }) {
+    const res = await fetch(`http://localhost:3000/api/posts/${query.id}`);
+    const data = await res.json();
+
+    return {
+      title: data.title,
+      body: data.body,
+      id: data.id,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt
+    }
+  };
+
+export default Post;
--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,11 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 const styles = {
+  header: {
+    fontSize: 30
+  },
   li: {
     margin: 10,
     padding: 20,
@@ -11,43 +15,42 @@ const styles = {
     border: '0.5px solid #DDD',
   },
   title: {
-    fontSize: 20,
+    fontSize: 22,
     fontStyle: 'bold'
   },
   preview: {
-    fontSize: 18
+    fontSize: 18,
+    opacity: 0.6
   }
 }
 
 const PostLink = (props) => (
-  <Link href={`/post?title=${props.title}&preview=${props.preview}`}>
+  // <Link href={`/post?title=${props.link.title}&preview=${props.link.body}`}>
+  <Link href={`/post?id=${props.link.id}`}>
     <li style={styles.li}>
-      <a style={styles.title}>{props.title}</a>
-      <p style={styles.preview}>{props.preview}</p>
+      <a style={styles.title}>{props.link.title}</a>
+      <p style={styles.preview}>{`${props.link.body.slice(0, 100)}...`}</p>
     </li>
   </Link>
 );
 
-
-const exampleLinks = [
-  {
-   title: 'Post 1',
-   preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel velit at erat posuere varius. Integer ultrices vestibulum leo, vitae dictum quam maximus eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque faucibus dolor vitae ornare venenatis. Cras feugiat justo in quam iaculis viverra. Morbi quam felis, facilisis sed elit vitae, sagittis finibus enim. Donec nec lacus quis nisi aliquam placerat. Duis cursus a neque quis egestas. Vestibulum convallis est sit amet augue dictum, vel accumsan turpis congue. Integer ante nisi, aliquam quis suscipit id, porttitor non augue.'
-  },
-  {
-   title: 'Post 2',
-   preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel velit at erat posuere varius. Integer ultrices vestibulum leo, vitae dictum quam maximus eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque faucibus dolor vitae ornare venenatis. Cras feugiat justo in quam iaculis viverra. Morbi quam felis, facilisis sed elit vitae, sagittis finibus enim. Donec nec lacus quis nisi aliquam placerat. Duis cursus a neque quis egestas. Vestibulum convallis est sit amet augue dictum, vel accumsan turpis congue. Integer ante nisi, aliquam quis suscipit id, porttitor non augue.'
-  },
-  {
-   title: 'Post 3',
-   preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel velit at erat posuere varius. Integer ultrices vestibulum leo, vitae dictum quam maximus eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque faucibus dolor vitae ornare venenatis. Cras feugiat justo in quam iaculis viverra. Morbi quam felis, facilisis sed elit vitae, sagittis finibus enim. Donec nec lacus quis nisi aliquam placerat. Duis cursus a neque quis egestas. Vestibulum convallis est sit amet augue dictum, vel accumsan turpis congue. Integer ante nisi, aliquam quis suscipit id, porttitor non augue.'
-  }
-];
-
-export default () => (
+const Posts = (props) => (
   <Layout>
     {/* TODO: list the posts here after pulling names from database */}
-    <p>Overviews of my projects, ordered by most recent first.</p>
-    { exampleLinks.map((link, idx) => <PostLink title={link.title} preview={link.preview} key={idx}/>) }
+    <h1 style={styles.header}>Projects, research, etc:</h1>
+    { props.links.map((link, idx) => <PostLink link={link} key={idx}/>) }
   </Layout>
 );
+
+Posts.getInitialProps = async function() {
+  const res = await fetch('http://localhost:3000/api/posts');
+  const data = await res.json();
+
+  console.log(`Show data fetched: ${data}`);
+  
+  return {
+    links: data
+  }
+};
+
+export default Posts;
